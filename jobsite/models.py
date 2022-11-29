ec7a665ec42a7ffac77a244a2d15571b0e5e290e
Dejun Jackson <dejunjackson16@gmail.com>
2022-11-28 23:21:23 -0600

    (Requires Migration) add jobsearch model

--- jobsite/models.py
+++ jobsite/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
 # Create your models here.
+class JobSearch(models.Model):
+    date_started = models.DateField()
+    date_ended = models.DateField()
+    name = models.CharField(max_length=255)
+
+    class Meta:
+        ordering = ['date_started']
